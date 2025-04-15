6c01f40a0ebbbd4b354c54ada2011898dc495984
Aditya Singh <aditya.krsingh0721@google.com>
2025-04-15 20:08:18 +0530

    added plop js to react app

--- plopFile.js
+++ plopFile.js
@@ -1,6 +1,17 @@
+import {pascalCase,camelCase,snakeCase,constantCase,capitalCase,dotCase,pathCase} from 'change-case'
+
 export default function (plop) {
+    //example my component name
+    plop.setHelper('pascalCase', text => pascalCase(text));   // MyComponentName
+    plop.setHelper('camelCase', text => camelCase(text));     // myComponentName
+    plop.setHelper('snakeCase', text => snakeCase(text));     // my_component_name
+    plop.setHelper('constantCase', text => constantCase(text)); // MY_COMPONENT_NAME
+    plop.setHelper('titleCase', text => capitalCase(text));   // My Component Name
+    plop.setHelper('dotCase', text => dotCase(text));         // my.component.name
+    plop.setHelper('pathCase', text => pathCase(text));       // my/component/name
+
     plop.setGenerator('basics', {
-        description: 'this is a skeleton plopfile',
+        description: 'this is to create a component',
         prompts: [
             {
                 type:"input",
@@ -11,9 +22,20 @@ export default function (plop) {
         actions: [
             {
                 type:'add',
-                path:'src/components/{{name}}.tsx',
-                templateFile:'templates/component.hbs'
-            }
+                path:'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+                templateFile:'templates/component/component.hbs'
+            },
+            {
+                type:'add',
+                path:'src/components/{{pascalCase name}}/{{pascalCase name}}.css',
+                templateFile:'templates/component/styles.hbs'
+            },
+            {
+                type:'add',
+                path:'src/components/{{pascalCase name}}/index.ts',
+                templateFile:'templates/component/index.hbs'
+            },
+
         ]  
     });
 };
